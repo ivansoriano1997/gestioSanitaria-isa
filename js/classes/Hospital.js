37b6f1301fc436c7fbd61960054412237cf5e886
Ivan Soriano <ivan.soriano@tusmedia.com>
2020-05-06 19:57:36 +0200

    afegida logica de crear hospital i mostrar pantalla ingressar pacients

--- js/classes/Hospital.js
+++ js/classes/Hospital.js
@@ -1,28 +1,25 @@
 'use strict'
 
 class Hospital {
-  constructor(nom, maximPacients, maximMetges) {
-    nomHospital = nom;
-    maximPacients = maximPacients;
-    pacientsIngressats = [];
-    ingressarPacient = ingressarPacient;
-    donarDaltaPacient = donarDaltaPacient;
-    morirPacient = morirPacient;
-  }
-  
+    constructor(nom, maximPacients, maximMetges) {
+        this.nomHospital = nom;
+        this.maximPacients = maximPacients;
+        this.pacientsIngressats = [];
+        this.maximMetges = maximMetges;
+    }
+
     ingressarPacient(pacient) {
-      if (pacient != null && (this.pacientsIngressats.length < this.maximPacients))
-        this.pacientsIngressats.push(pacient);
+        if (pacient != null && (this.pacientsIngressats.length < this.maximPacients))
+            this.pacientsIngressats.push(pacient);
     }
 
     donarDaltaPacient(llitPacient) {
-      alert("S'ha donat d'alta el pacient " + this.pacientsIngressats[llitPacient].nomHospital + " de la malaltia " + this.pacientsIngressats[llitPacient].malaltia);
-      this.pacientsIngressats[llitPacient] = {};
+        alert("S'ha donat d'alta el pacient " + this.pacientsIngressats[llitPacient].nomHospital + " de la malaltia " + this.pacientsIngressats[llitPacient].malaltia);
+        this.pacientsIngressats[llitPacient] = {};
     }
 
     morirPacient(llitPacient) {
-      alert("Encara que s'ha fet tot el que s'ha pogut, el pacient " + this.pacientsIngressats[llitPacient].nomHospital + " ha mort de la malaltia " + this.pacientsIngressats[llitPacient].malaltia);
-      this.pacientsIngressats[llitPacient] = {};
+        alert("Encara que s'ha fet tot el que s'ha pogut, el pacient " + this.pacientsIngressats[llitPacient].nomHospital + " ha mort de la malaltia " + this.pacientsIngressats[llitPacient].malaltia);
+        this.pacientsIngressats[llitPacient] = {};
     }
-
 }
--- js/main.js
+++ js/main.js
@@ -20,6 +20,8 @@ const eleID_btnGestAplicacio = document.getElementById("btnGestAplicacio");
 
 const eleID_h2ResutltatFormControls = document.getElementById("h2ResutltatFormControls");
 
+let hospital = {};
+
 /*
 taula_missatges[0][intTitol]="No habilitat"
 taula_missatges[0][intMissatge]="Ho sento, encara no està habilitat!"
@@ -34,107 +36,108 @@ taula_missatges[3][intTitol]="Camp buit"
 taula_missatges[3][intMissatge]="Ho sento, encara no està habilitat!"
  */
 
-const taula_missatges=[[]];
+const taula_missatges = [
+    []
+];
 
 const intTitol = 1
 const intMissatge = 2;
 
-taula_missatges[0]=[];
-taula_missatges[0][intTitol]="No habilitat";
-taula_missatges[0][intMissatge]="Ho sento, encara no està habilitat!";
+taula_missatges[0] = [];
+taula_missatges[0][intTitol] = "No habilitat";
+taula_missatges[0][intMissatge] = "Ho sento, encara no està habilitat!";
 
-taula_missatges[1]=[];
-taula_missatges[1][intTitol]="El camp màxim pacients és obligatori!";
-taula_missatges[1][intMissatge]="Cal que entris un nombre al camp màxim pacients!";
+taula_missatges[1] = [];
+taula_missatges[1][intTitol] = "El camp màxim pacients és obligatori!";
+taula_missatges[1][intMissatge] = "Cal que entris un nombre al camp màxim pacients!";
 
-taula_missatges[2]=[];
-taula_missatges[2][intTitol]="El camp Nom Hospital és obligatori!";
-taula_missatges[2][intMissatge]="Cal que entris un nom al camp Hospital!";
+taula_missatges[2] = [];
+taula_missatges[2][intTitol] = "El camp Nom Hospital és obligatori!";
+taula_missatges[2][intMissatge] = "Cal que entris un nom al camp Hospital!";
 
 //    let metge = new Metge('Ivan', 'Soriano', '12345678A', 'Oncologia');
 //     alert(JSON.stringify(metge));
 
 function comprovaCampBuit(objecteRebut) {
-      if (objecteRebut.value == "") {
-            mostraMissatge(2);
-      }
-      console.log(objecteRebut.value);
+    if (objecteRebut.value == "") {
+        mostraMissatge(2);
+    }
 }
 
 function comprovaCampNumero(objecteRebut) {
-      switch ( isNaN(objecteRebut.value) ? 1 : objecteRebut.value=="" ? 2 : 0 ) {
-            case 1:
-                  mostraMissatge(1);
-                  break;
+    switch (isNaN(objecteRebut.value) ? 1 : objecteRebut.value == "" ? 2 : 0) {
+        case 1:
+            mostraMissatge(1);
+            break;
 
-            case 2:
-                  mostraMissatge(2);
-                  break;
+        case 2:
+            mostraMissatge(2);
+            break;
 
-      }
-      // if (isNaN(objecteRebut.value) || (objecteRebut.value=="")) 
-      //   {
-      //     alert("Must input numbers");
-      //     return false;
-      //   }
+    }
+    // if (isNaN(objecteRebut.value) || (objecteRebut.value==""))
+    //   {
+    //     alert("Must input numbers");
+    //     return false;
+    //   }
 }
 
 
 function mostraMissatge(codiMissatge) {
-      /* Amb format JQuery   */
-      $('#divEspaiModal').find('#titolModal').text(taula_missatges[codiMissatge][intTitol]);
-      $('#divEspaiModal').find('#missatgeModal').text(taula_missatges[codiMissatge][intMissatge]);
-      $('#divEspaiModal').modal('show');
+    /* Amb format JQuery   */
+    $('#divEspaiModal').find('#titolModal').text(taula_missatges[codiMissatge][intTitol]);
+    $('#divEspaiModal').find('#missatgeModal').text(taula_missatges[codiMissatge][intMissatge]);
+    $('#divEspaiModal').modal('show');
 }
 
 function mostraBotons() {
-      eleID_btnGestHospitals.disabled = false;
-      eleID_btnGestPacients.disabled = false;
-      eleID_btnGestMalalties.disabled = false;
-      eleID_btnGestMetges.disabled = false;
-      eleID_btnGestAplicacio.disabled = false;
+    eleID_btnGestHospitals.disabled = false;
+    eleID_btnGestPacients.disabled = false;
+    eleID_btnGestMalalties.disabled = false;
+    eleID_btnGestMetges.disabled = false;
+    eleID_btnGestAplicacio.disabled = false;
 
-      $(eleID_btnGestHospitals).removeClass('btn-dark');
-      $(eleID_btnGestHospitals).addClass('btn-primary');
+    $(eleID_btnGestHospitals).removeClass('btn-dark');
+    $(eleID_btnGestHospitals).addClass('btn-primary');
 
-      $(eleID_btnGestPacients).removeClass('btn-dark');
-      $(eleID_btnGestPacients).addClass('btn-primary');
+    $(eleID_btnGestPacients).removeClass('btn-dark');
+    $(eleID_btnGestPacients).addClass('btn-primary');
 
-      $(eleID_btnGestMalalties).removeClass('btn-dark');
-      $(eleID_btnGestMalalties).addClass('btn-primary');
+    $(eleID_btnGestMalalties).removeClass('btn-dark');
+    $(eleID_btnGestMalalties).addClass('btn-primary');
 
-      $(eleID_btnGestMetges).removeClass('btn-dark');
-      $(eleID_btnGestMetges).addClass('btn-primary');
+    $(eleID_btnGestMetges).removeClass('btn-dark');
+    $(eleID_btnGestMetges).addClass('btn-primary');
 
-      $(eleID_btnGestAplicacio).removeClass('btn-dark');
-      $(eleID_btnGestAplicacio).addClass('btn-primary');
+    $(eleID_btnGestAplicacio).removeClass('btn-dark');
+    $(eleID_btnGestAplicacio).addClass('btn-primary');
 }
 
 function amagaBotons() {
-      eleID_btnGestHospitals.disabled = true;
-      eleID_btnGestPacients.disabled = true;
-      eleID_btnGestMalalties.disabled = true;
-      eleID_btnGestMetges.disabled = true;
-      eleID_btnGestAplicacio.disabled = true;
+    eleID_btnGestHospitals.disabled = true;
+    eleID_btnGestPacients.disabled = true;
+    eleID_btnGestMalalties.disabled = true;
+    eleID_btnGestMetges.disabled = true;
+    eleID_btnGestAplicacio.disabled = true;
 
-      $(eleID_btnGestHospitals).removeClass('btn-primary');
-      $(eleID_btnGestHospitals).addClass('btn-dark');
+    $(eleID_btnGestHospitals).removeClass('btn-primary');
+    $(eleID_btnGestHospitals).addClass('btn-dark');
 
-      $(eleID_btnGestPacients).removeClass('btn-primary');
-      $(eleID_btnGestPacients).addClass('btn-dark');
+    $(eleID_btnGestPacients).removeClass('btn-primary');
+    $(eleID_btnGestPacients).addClass('btn-dark');
 
-      $(eleID_btnGestMalalties).removeClass('btn-primary');
-      $(eleID_btnGestMalalties).addClass('btn-dark');
+    $(eleID_btnGestMalalties).removeClass('btn-primary');
+    $(eleID_btnGestMalalties).addClass('btn-dark');
 
-      $(eleID_btnGestMetges).removeClass('btn-primary');
-      $(eleID_btnGestMetges).addClass('btn-dark');
+    $(eleID_btnGestMetges).removeClass('btn-primary');
+    $(eleID_btnGestMetges).addClass('btn-dark');
 
-      $(eleID_btnGestAplicacio).removeClass('btn-primary');
-      $(eleID_btnGestAplicacio).addClass('btn-dark');
+    $(eleID_btnGestAplicacio).removeClass('btn-primary');
+    $(eleID_btnGestAplicacio).addClass('btn-dark');
 }
 
 
-/* https://developer.mozilla.org/en-US/docs/Web/API/Element/classList 
+/* https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
 document.getElementById("MyElement").classList.add('MyClass');
 
 document.getElementById("MyElement").classList.remove('MyClass');
@@ -150,83 +153,187 @@ https://stackoverflow.com/questions/195951/how-can-i-change-an-elements-class-wi
 */
 
 function conteClass(element, nomClass) {
-      return (' ' + element.classList + ' ').indexOf(' ' + nomClass + ' ') > -1;
-  }
+    return (' ' + element.classList + ' ').indexOf(' ' + nomClass + ' ') > -1;
+}
 
 
 
 function eliminaClass(elementRebut, nomClass) {
-      var arrLlistaClasses;
-      
-      arrLlistaClasses = elementRebut.className.split(" ");
-      if (arrLlistaClasses.indexOf(nomClass) == -1) {  // ens indica que no existeix
-            elementRebut.className += " " + nomClass;
-      }
+    var arrLlistaClasses;
+
+    arrLlistaClasses = elementRebut.className.split(" ");
+    if (arrLlistaClasses.indexOf(nomClass) == -1) { // ens indica que no existeix
+        elementRebut.className += " " + nomClass;
+    }
 }
 
-    function afegirClass(elementRebut, nomClass) {
-      var arrLlistaClasses;
-      
-      arrLlistaClasses = elementRebut.className.split(" ");
-      if (arrLlistaClasses.indexOf(nomClass) == -1) {  // ens indica que no existeix
-            elementRebut.className += " " + nomClass;
-      }
+function afegirClass(elementRebut, nomClass) {
+    var arrLlistaClasses;
+
+    arrLlistaClasses = elementRebut.className.split(" ");
+    if (arrLlistaClasses.indexOf(nomClass) == -1) { // ens indica que no existeix
+        elementRebut.className += " " + nomClass;
     }
-
-
-    
-    function mostraGestioApp(objecte){
-      eleID_divPresentacio.classList.toggle("d-none");
-      eleID_divControls.classList.toggle("d-none");
-      amagaBotons();
-   }
-
-
-    function mostraGestioMetges(objecte){
-          mostraMissatge(0);
-    }
-
-    function mostraGestioMalaties(objecte){
-       mostraMissatge(0);
-    }
-    
-
-
-function mostraGestioHospital(objecte){
-      document.getElementById('tancaDivHospital').onclick = function tanca() {
-            eleID_divPresentacio.classList.toggle("d-none");
-            eleID_divHospital.classList.toggle("d-none");
-            mostraBotons();
-      }
-      eleID_divPresentacio.classList.toggle("d-none");
-      eleID_divHospital.classList.toggle("d-none");
-      amagaBotons();
-   }
-
-function ocultaGestioHospital(objecteRebut){
-            eleID_divPresentacio.classList.toggle("d-none");
-            eleID_divHospital.classList.toggle("d-none");
-            mostraBotons();
-   }
-
-function mostraGestioPacients(objecteRebut){
-      if (!conteClass(eleID_divPacient,"d-none")) {
-            afegirClass(eleID_divPacient,"d-none");
-      }
-      eleID_divPresentacio.classList.toggle("d-none");
-      eleID_divPacient.classList.toggle("d-none");
-      amagaBotons();
 }
 
-function ocultaGestioPacients(objecteRebut){
-      eleID_divPresentacio.classList.toggle("d-none");
-      eleID_divPacient.classList.toggle("d-none");
-      mostraBotons();
-   }
 
-   function ocultaControls(objecteRebut){
-      eleID_divPresentacio.classList.toggle("d-none");
-      eleID_divControls.classList.toggle("d-none");
-      mostraBotons();
-   }
-   
+
+function mostraGestioApp(objecte) {
+    eleID_divPresentacio.classList.toggle("d-none");
+    eleID_divControls.classList.toggle("d-none");
+    amagaBotons();
+}
+
+
+function mostraGestioMetges(objecte) {
+    mostraMissatge(0);
+}
+
+function mostraGestioMalaties(objecte) {
+    mostraMissatge(0);
+}
+
+
+
+function mostraGestioHospital(objecte) {
+    document.getElementById('tancaDivHospital').onclick = function tanca() {
+        eleID_divPresentacio.classList.toggle("d-none");
+        eleID_divHospital.classList.toggle("d-none");
+        mostraBotons();
+    }
+    eleID_divPresentacio.classList.toggle("d-none");
+    eleID_divHospital.classList.toggle("d-none");
+    amagaBotons();
+}
+
+function ocultaGestioHospital(objecteRebut) {
+    eleID_divPresentacio.classList.toggle("d-none");
+    eleID_divHospital.classList.toggle("d-none");
+    mostraBotons();
+}
+
+// CREA L'INPUT DEL NOM DEL PACIENT.
+function crearInputNomPacient(numeroPacient) {
+  let inputNomPacient = document.createElement("input")
+  inputNomPacient.setAttribute("type", "text");
+  inputNomPacient.setAttribute("name", "inputNomPacient" + numeroPacient.toString());
+  inputNomPacient.classList.add("form-control");
+
+  return inputNomPacient;
+}
+
+// CREA L'OPTION DEL SELECT DE MALALTIES
+function crearOptionSelectMalalties(numeroMalaltia) {
+  let optionMalaltia = document.createElement("option");
+  optionMalaltia.value = "malaltia" + numeroMalaltia.toString();
+  optionMalaltia.text = numeroMalaltia >= 0 ? llistaMalalties[numeroMalaltia] : "Cap malaltia";
+
+  if (numeroMalaltia < 0)
+    optionMalaltia.title = "Utilizar per quan sigui una revisió o quan es tracti d'una malaltia desconeguda.";
+
+  return optionMalaltia;
+}
+
+// CREA EL SELECT DE MALALTIES
+function crearSelectMalalties(numeroPacient) {
+  // Creem el desplegable de materies
+  let selectMalalties = document.createElement("select");
+  selectMalalties.id = "selectMalaltiaPacient" + numeroPacient.toString();
+  selectMalalties.classList.add("form-control");
+
+  // Creem la opció de 'cap seleccionada'
+  selectMalalties.appendChild(crearOptionSelectMalalties(-1));
+
+  // Afegim les opcions al formulari
+  for (let numeroMalaltia = 0; numeroMalaltia < llistaMalalties.length; numeroMalaltia++) {
+    selectMalalties.appendChild(crearOptionSelectMalalties(numeroMalaltia));
+  }
+
+  return selectMalalties;
+}
+
+// CREA EL CONTINGUT DEl FORMULARI DE PACIENT
+function crearContingutDivPacient(numeroPacient) {
+  let formulariPacient = null;
+
+  // Creem el contenidor "row" per al pacient
+  let divRowPacient = document.createElement("div");
+  divRowPacient.classList.add("row");
+
+  // Creem el contenidor "col" per al input del nom del pacient
+  let divColNomPacient = document.createElement("div");
+  divColNomPacient.classList.add("col");
+  divColNomPacient.classList.add("mb-3");
+
+  // Afegim la label del nom del pacient
+  let labelPacient = document.createElement("label");
+  labelPacient.innerText = "Introdueix el nom del pacient:";
+  labelPacient.setAttribute("for", "inputNomPacient" + numeroPacient.toString());
+  divColNomPacient.appendChild(labelPacient);
+
+  // Afegim l'input del nom del pacient
+  divColNomPacient.appendChild(crearInputNomPacient(numeroPacient));
+
+  // Afegim el contenidor "col" dins del contenidor "row"
+  divRowPacient.appendChild(divColNomPacient);
+
+  // Creem el contenidor "col" per al input del nom del pacient
+  let divColMalaltiaPacient = document.createElement("div");
+  divColMalaltiaPacient.classList.add("col");
+  divColMalaltiaPacient.classList.add("mb-3");
+
+  // Afegim la label del nom del pacient
+  let labelMalaltia = document.createElement("label");
+  labelMalaltia.innerText = "Introdueix el nom del pacient:";
+  labelMalaltia.setAttribute("for", "inputNomPacient" + numeroPacient.toString());
+  divColMalaltiaPacient.appendChild(labelMalaltia);
+
+  // Afegim el select de malalties
+  divColMalaltiaPacient.appendChild(crearSelectMalalties(numeroPacient));
+
+  // Afegim el contenidor "col" dins del contenidor "row"
+  divRowPacient.appendChild(divColMalaltiaPacient);
+
+  // Afegim el contenidor del pacient al formulari
+  document.getElementById("dadesPacient").appendChild(divRowPacient);
+}
+
+function mostraGestioPacients(objecteRebut) {
+    for (let pacient = 0; pacient < hospital.maximPacients; pacient++) {
+      crearContingutDivPacient(pacient);
+    }
+
+    if (!conteClass(eleID_divPacient, "d-none")) {
+        afegirClass(eleID_divPacient, "d-none");
+    }
+    eleID_divPresentacio.classList.toggle("d-none");
+    eleID_divPacient.classList.toggle("d-none");
+    amagaBotons();
+}
+
+function ocultaGestioPacients(objecteRebut) {
+    eleID_divPresentacio.classList.toggle("d-none");
+    eleID_divPacient.classList.toggle("d-none");
+    mostraBotons();
+}
+
+function ocultaControls(objecteRebut) {
+    eleID_divPresentacio.classList.toggle("d-none");
+    eleID_divControls.classList.toggle("d-none");
+    mostraBotons();
+}
+
+function crearHospital() {
+    // Creem l'objecte Hospital amb les dades introduïdes formulari
+    hospital = new Hospital (
+      document.getElementById("inputNomHospital").value,
+      document.getElementById("maximPacientsHospital").value,
+      document.getElementById("maximMetgesHospital").value
+    );
+
+    // Ocultem aquest formulari
+    ocultaGestioHospital();
+
+    // Mostrem el formulari de gestió de pacients
+    mostraGestioPacients();
+}
