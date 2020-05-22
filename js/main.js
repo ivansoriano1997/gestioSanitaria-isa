fb2fd7474134845f2c3bbcf2cdc5b4308f8abfd5
Ivan Soriano Arabia <ivansoriano1997@ginebro.cat>
2020-05-22 09:59:30 +0200

    Solucionat bug amb el nom de l'Hospital quan ja no hi ha cap pacient ingressat

--- js/main.js
+++ js/main.js
@@ -518,7 +518,7 @@ function pacientsEnTractamentDonarDalta(llitPacient) {
   document.getElementById("divPacientEnTractament" + llitPacient.toString()).remove();
 
   if (totsLlitsBuits())
-  dadesPacientTractament.innerHTML = ('<p class="text-center">L\'Hospital ' + hospital.nom + ' no té cap pacient ingressat en aquests moments.</p>' +
+  dadesPacientTractament.innerHTML = ('<p class="text-center">L\'Hospital ' + hospital.nomHospital + ' no té cap pacient ingressat en aquests moments.</p>' +
   '<div class="text-center">' +
     '<button type="button" class="btn btn-primary mt-4" onClick="window.location.reload()">Tornar a començar</button>' +
   '</div>');
@@ -529,7 +529,7 @@ function pacientsEnTractamentMorir(llitPacient) {
   document.getElementById("divPacientEnTractament" + llitPacient.toString()).remove();
 
   if (totsLlitsBuits())
-    dadesPacientTractament.innerHTML = ('<p class="text-center">L\'Hospital ' + hospital.nom + ' no té cap pacient ingressat en aquests moments.</p>' +
+    dadesPacientTractament.innerHTML = ('<p class="text-center">L\'Hospital ' + hospital.nomHospital + ' no té cap pacient ingressat en aquests moments.</p>' +
     '<div class="text-center">' +
       '<button type="button" class="btn btn-primary mt-4" onClick="window.location.reload()">Tornar a començar</button>' +
     '</div>');
